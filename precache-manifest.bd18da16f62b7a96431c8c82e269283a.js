837a36835430ff50133fad4000f1b134d6ac1bd3
harmanparihar <harmanparihar18@gmail.com>
2018-10-10 20:10:25 -0700

    Updates

---
+++ precache-manifest.bd18da16f62b7a96431c8c82e269283a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "748513f2188d10425871",
+    "url": "/react-dashboard/static/js/runtime~main.748513f2.js"
+  },
+  {
+    "revision": "4aa8227a758550d14b41",
+    "url": "/react-dashboard/static/js/main.4aa8227a.chunk.js"
+  },
+  {
+    "revision": "bc19effb4d9b658f106d",
+    "url": "/react-dashboard/static/js/1.bc19effb.chunk.js"
+  },
+  {
+    "revision": "4aa8227a758550d14b41",
+    "url": "/react-dashboard/static/css/main.8211ea36.chunk.css"
+  },
+  {
+    "revision": "b37cb9e01ec0669e94d53616df08058a",
+    "url": "/react-dashboard/index.html"
+  }
+];
